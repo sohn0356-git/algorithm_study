c8af3d9aa34bbc14aafad8c98ae01fcff329cc3c
cano721 <cano721@naver.com>
2021-03-21 20:41:05 +0900

    Update 곱셈

--- Haebum/초급반/bj3085_사탕게임.py
+++ Haebum/초급반/bj3085_사탕게임.py
@@ -1,7 +1,7 @@
 import copy
 #정답의 최대는 50
 
-#shallow copy deep copy
+#shallow copy deep copy (정리하기!!)
 
 #n*n의 그래프에서 서로 다른 사탕을 한번 바꿀 수 있다
 #그때 같은 사탕으로 이루어진 가장 긴 줄(행 또는 열)을 골라
@@ -12,15 +12,13 @@ import copy
 #가로방향,세로방향 사탕 바꾸기 후
 #연속된 같은 사탕 갯수 세기
 
+#최대값 최소값 얼마정도인지
 #시간복잡도는 50**4 5**4 * 10 **4 가능
-#공간복잡도는 
+#공간복잡도는 파악... n이 50까지가능 char이므로 1byte 50*50 = 2500byte = 2.5kb
 
 def changeGraph(graph):
-    answerCheck(graph)
-    for i in range(n):
+    for i in range(n-1):
         for j in range(n):
-            if i==n-1:
-                continue
             if graph[i][j] != graph[i+1][j]:
                 graph[i][j], graph[i+1][j] = graph[i+1][j], graph[i][j]
                 answerCheck(graph)
@@ -36,17 +34,23 @@ def answerCheck(sugar):
     for k in range(n):
         witdhCnt=1
         heightCnt=1
-        for l in range(n):
-            if l==n-1:
-                continue
+        for l in range(n-1):
             if sugar[l][k] == sugar[l+1][k]:
                 heightCnt += 1
+            else:
+                if answer < heightCnt:
+                    answer = heightCnt
+                heightCnt = 1
             if sugar[k][l] == sugar[k][l+1]:
                 witdhCnt += 1
-        if answer < witdhCnt:
-            answer = witdhCnt
+            else:
+                if answer < witdhCnt:
+                    answer = witdhCnt
+                witdhCnt = 1 
         if answer < heightCnt:
             answer = heightCnt
+        if answer < witdhCnt:
+            answer = witdhCnt
 
 n = int(input())
 graph = []
