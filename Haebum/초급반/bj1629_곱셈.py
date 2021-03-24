c0727b6c22e8306d6e1efade95a5fe3e4751ae08
cano721 <cano721@naver.com>
2021-03-24 23:46:45 +0900

    Update 섬의개수

--- Haebum/초급반/bj1629_곱셈.py
+++ Haebum/초급반/bj1629_곱셈.py
@@ -4,10 +4,12 @@
 # 재귀함수를 사용한다...(여기까지 생각 성공)
 
 # 실패해서 구글링을 통해 방법 확인..
-# b를 짝수일때와 홀수일때로 나누고 짝수이면 (b//2)**2 식으로 곱셈으 수를 줄임
+# b를 짝수일때와 홀수일때로 나누고 짝수이면 (b//2)**2 식으로 곱셈의 수를 줄임
 # 예시 10**10 b 짝수-> (10**5)**2
 # 예시 10**11 b 홀수-> (10**5)**2*10
 #분할정복 문제라고 함
+#시간복잡도는 log(b)
+#공간복잡도는 <=c
 
 def solve(a,b):
     if b == 0:
@@ -15,10 +17,14 @@ def solve(a,b):
     elif b == 1:
         return a
     elif b%2==1:
+        print(b)
         return solve(a,b-1)*a
+    print(b)
     result = solve(a,b//2)
     result %=c
     return (result**2)%c
 
 a, b, c = map(int,input().split())
 print(solve(a,b)%c)
+
+
--- Haebum/초급반/codingTest.py
+++ Haebum/초급반/codingTest.py
@@ -1,22 +1,16 @@
-#재귀함수
-def factorial(n):
-    if n == 1:
-        return 1
-    return n*factorial(n-1)
+a = [1,2,[4,5,6]]
+b= a   #shallow copy
+print(b) # [1,2,3]
+b[1] = 3    # b의 1번자리를 3으로 변경
+print(b) # [1,3,3]
+b[2]
+print(a) # [1,3,3] b를 변경했지만 a도 같이 변경됨!
 
+import copy
 
-#실행문
-factorial(4)
-
-실행하게되면 처음에
-factorial(4) 를 실행해서 4*factorial(3) 을 리턴받게 된다.
-
-리턴받은 factorial(3)은 다시 3*factorial(2)를 리턴받고
-factorial(2)는 2*factorial(1)을 리턴.
-
-factorial(1)은 탈출조건인 if n==1을 성립하면서
-더 이상 factorial함수를 부르지않고 1로 리턴된다.
-
-그럼 4*3*2*1이 남게 되어 답은 24가 된다.
-
-4*factorial(3) 일때 factorial(3)의 자리에 3*factorial(2)가 들어간다고 보면 된다.
+a = [1,2,[4,5,6]]
+b = copy.deepcopy(a)
+print(b)
+b[1] = 2
+print(b)
+print(a)
