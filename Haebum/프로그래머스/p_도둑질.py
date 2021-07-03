69a2f7d450d7fa6624bdf2ff49c9a20b6c36892b
cano721 <cano721@naver.com>
2021-07-03 16:39:07 +0900

    알고리즘 코드 주석

--- Haebum/프로그래머스/p_도둑질.py
+++ Haebum/프로그래머스/p_도둑질.py
@@ -1,3 +1,8 @@
+#dp문제
+#rgb거리와 유사
+#한쪽으로 정하고 넘어가면 되는데 맨처음집에 의해 마지막집도 결정되니
+#처음집을 훔쳤냐 안훔쳤냐로 나눠서 dp 돌리기
+#[][] 1일경우 훔친거 0일경우 안훔친거 뒤의 []은 집
 def solution(money):
     answer = 0
     homes = len(money)
@@ -11,7 +16,8 @@ def solution(money):
         firstNot[0][steal] = max(firstNot[1][steal-1],firstNot[0][steal-1])
         firstSteal[1][steal] = firstSteal[0][steal-1] + money[steal]
         firstNot[1][steal] = firstNot[0][steal-1] + money[steal]
-    
+    #누적형태이므로 마지막이 정답
+    #max(처음집 훔치기[마지막집 안훔침], 처음집 안훔침[마지막집 훔침],처음집 안훔침[마지막집 안훔침])
     answer = max(firstSteal[0][homes-1],firstNot[1][homes-1],firstNot[0][homes-1])
     print(answer)
     return answer
