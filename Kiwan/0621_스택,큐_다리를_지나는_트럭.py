5e1694cf13ce81ac294f9b3555ed622ba1c373d9
gioan92 <gioan92@naver.com>
2021-07-16 23:45:34 +0900

    프로그래머스 주식가격 업로드

---
+++ Kiwan/0621_스택,큐_다리를_지나는_트럭.py
@@ -0,0 +1,30 @@
+## 다리를 지나는 트럭
+
+bridge_length = 2
+weight = 10
+truck_weights = [7,4,5,6]
+# bridge_length = 100
+# weight = 100
+# truck_weights = [10]
+# bridge_length = 100
+# weight = 100
+# truck_weights = [10,10,10,10,10,10,10,10,10,10]
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+
+    return answer
+
+# 0
+# 1 10
+# 2 10
+# 3 10
+# 4 10
+# 5 10
+# 6 10
+# 7 10
+# 8 10
+# 9 10
+# 10 10
+# ...
+# 100 10
+# 101 없음
