8360e459820ebf532b8282dc410ddbf936634f14
kiwanpark <gioan92@naver.com>
2021-07-17 13:59:44 +0900

    프로그래머스 다리를지나는트럭 업로드

--- Kiwan/0621_스택,큐_다리를_지나는_트럭.py
+++ Kiwan/0621_스택,큐_다리를_지나는_트럭.py
@@ -1,18 +1,36 @@
 ## 다리를 지나는 트럭
-
-bridge_length = 2
-weight = 10
-truck_weights = [7,4,5,6]
-# bridge_length = 100
-# weight = 100
-# truck_weights = [10]
-# bridge_length = 100
-# weight = 100
-# truck_weights = [10,10,10,10,10,10,10,10,10,10]
+from collections import deque
 def solution(bridge_length, weight, truck_weights):
     answer = 0
+    q = deque()
+    total_weight = 0
+    idx = 0
+    N = len(truck_weights)
 
+    now = 1
+    q.append([now, truck_weights[idx]])
+    total_weight += truck_weights[idx]
+    idx += 1
+    while True:
+        now += 1
+        if q:
+            in_time, truck_weight = q.popleft()
+            total_weight -= truck_weight
+            if now - in_time < bridge_length:
+                q.appendleft([in_time, truck_weight])
+                total_weight += truck_weight
+            if idx < N and total_weight + truck_weights[idx] <= weight:
+                q.append([now, truck_weights[idx]])
+                total_weight += truck_weights[idx]
+                idx += 1
+        else:
+            break
+    answer = now - 1
     return answer
+
+#print(solution(2,10,[7,4,5,6]))
+#print(solution(100,100,[10]))
+print(solution(100,100,[10,10,10,10,10,10,10,10,10,10]))
 
 # 0
 # 1 10
